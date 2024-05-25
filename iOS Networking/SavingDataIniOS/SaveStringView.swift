3297b9d61dbf16b36f9228d2aa6458963f6afa84
abdulwariskhan <hellokhan.ch@gmail.com>
2024-05-25 19:11:00 +0200

    Save File in iOS

---
+++ iOS Networking/SavingDataIniOS/SaveStringView.swift
@@ -0,0 +1,18 @@
+//
+//  SaveStringView.swift
+//  iOS Networking
+//
+//  Created by Khan on 25.05.2024.
+//
+
+import SwiftUI
+
+struct SaveStringView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SaveStringView()
+}
