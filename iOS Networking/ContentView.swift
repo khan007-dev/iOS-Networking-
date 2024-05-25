5c10ab04c96fa49f79f8e34c0f04881250e452f9
abdulwariskhan <hellokhan.ch@gmail.com>
2024-05-25 16:48:25 +0200

    Initial Commit

---
+++ iOS Networking/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS Networking
+//
+//  Created by Khan on 25.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS Networking/iOS_NetworkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_NetworkingApp.swift
+//  iOS Networking
+//
+//  Created by Khan on 25.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct iOS_NetworkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
